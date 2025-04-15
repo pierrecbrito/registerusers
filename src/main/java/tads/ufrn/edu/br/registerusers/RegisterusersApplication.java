c4eb70ad6689c884e80d271d3394a8f66eb617a9
Pierre Brito <carlospierre07@gmail.com>
2025-04-15 14:21:29 -0300

    first commit

---
+++ src/main/java/tads/ufrn/edu/br/registerusers/RegisterusersApplication.java
@@ -0,0 +1,13 @@
+package tads.ufrn.edu.br.registerusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterusersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tads/ufrn/edu/br/registerusers/RegisterusersApplicationTests.java
@@ -0,0 +1,13 @@
+package tads.ufrn.edu.br.registerusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
